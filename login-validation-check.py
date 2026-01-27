4bfa10b7c533761fa56bcdeeb6745def791a77ae
newbeetoveteran <rahul.singh04@zohomail.in>
2026-01-27 11:41:01 +0530

    Program to check the user credentials using if-else statement

---
+++ login-validation-check.py
@@ -0,0 +1,13 @@
+
+
+user_input_username = input("Enter the username: ")
+user_input_password = input("Enter the password: ")
+username_val = ("newbeetoveteran")
+password_val = ("123456@abc")
+
+
+if user_input_username == username_val and user_input_password == password_val:
+    print("Login Successful!")
+
+else:
+    print("Incorrect credentials entered. Please check your details and enter again.")
