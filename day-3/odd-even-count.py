f40be15e85ab106b197f959d877eed93e376c543
newbeetoveteran <rahul.singh04@zohomail.in>
2026-01-28 18:08:03 +0530

    added python program to count odd even

---
+++ day-3/odd-even-count.py
@@ -0,0 +1,15 @@
+
+
+user_input = int(input("Enter the number: "))
+even_count = 0
+odd_count = 0
+
+for i in range(0, user_input):
+    if i%2 == 0:
+        even_count += 1
+    print(f"Total even numbers between 0 and {user_input} is: {ceven_count}")
+
+else:
+    if i%2 != 0:
+        odd_count += 1
+    print(f"Total odd numbers between 0 and {user_input} is: {odd_count}")
