0eb126d0eaed49709e95bb6d59436d50c6565dc4
newbeetoveteran <rahul.singh04@zohomail.in>
2026-01-29 22:17:00 +0530

    Python program to print string in new line without space

---
+++ day-4/charecter-print-without-space.py
@@ -0,0 +1,6 @@
+#printing each character of a string on a new line without space
+#concept: using for loop & string
+
+user_input = input("Enter the word: ")
+for alphabet in user_input:
+    print(alphabet)
