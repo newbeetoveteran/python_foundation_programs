5ac2300673b35f7ab1ff18ba9d580beab5dd0c17
newbeetoveteran <rahul.singh04@zohomail.in>
2026-01-29 16:57:18 +0530

    Removed file after moving them to respective folder

--- Odd-even-check.py
+++
@@ -1,7 +0,0 @@
-print("Hi there! This program will let you check odd or even numbers.")
-user_input = int(input("Enter the number: "))
-
-if user_input % 2 ==0:
-    print("The number entered is  even!")
-else:
-    print("The number entered is Odd!")
--- README.md
+++
@@ -1,7 +0,0 @@
-Hi there!!
-
-This repository is a collection of all the initial struggles of any programmer.
-The aim here is to document my initial learning path through practice.
-
-Message to new programmers-- 
-Keep pushing!!
