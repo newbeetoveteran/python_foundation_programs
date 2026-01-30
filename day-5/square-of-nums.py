dd7f2a50f69114881dc8e4faaeb16b84fbd97842
newbeetoveteran <rahul.singh04@zohomail.in>
2026-01-30 12:16:03 +0530

    Python program to print sqauare of numbers between 1- user_input

---
+++ day-5/square-of-nums.py
@@ -0,0 +1,8 @@
+#python program to print the square of each number from 1 to 10
+#concept : usage of for loop
+
+
+user_input = int(input{"Enter a number between 1-10: "})
+for i in range int(1, user_input+1):
+    i = i**2
+    print (i)
