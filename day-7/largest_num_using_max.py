b1cf67870d53f06932cd0ab50dd17cfbaab611c2
newbeetoveteran <rahul.singh04@zohomail.in>
2026-02-02 22:47:52 +0530

    Program to print largest number in a list using max

---
+++ day-7/largest_num_using_max.py
@@ -0,0 +1,6 @@
+# Python program for cheching largest number in a list using max
+# Usage of max function
+
+num_list = [23, 45, 12, 67, 34, 89, 90]
+largest_nm = max(num_list)
+print(f"The largest number in the list is: {largest_nm}")
