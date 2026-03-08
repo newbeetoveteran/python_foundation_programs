b4fcc4057ebc173c6e5c163857cc21922273fbc9
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-08 13:28:20 +0530

    largest number in a list

---
+++ working_with_lists/largest_in_list.py
@@ -0,0 +1,8 @@
+#find the largest number in the list
+
+input_list = [5, 8, 2, 9, 1]
+largest_num = 0
+for i in input_list:
+    if i > largest_num:
+        largest_num = i
+print(f"Largest number in {input_list} is {largest_num}")
