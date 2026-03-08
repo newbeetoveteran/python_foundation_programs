4c9fd690de1ba0805ef7cc91e2d8c7757144dc1c
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-08 13:57:03 +0530

    counting number of even digits in a given list of numbers

---
+++ working_with_lists/even_num_count.py
@@ -0,0 +1,8 @@
+#even number count in a list
+
+sample_list = [2, 5, 8, 7, 10]
+count = 0
+for i in sample_list:
+    if i % 2 == 0:
+        count += 1
+print(f"Total number of even digits in {sample_list} is {count}")
