9d6e1c829828c24704090ce8db3c8e35a47b1b2d
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-08 12:14:31 +0530

    Modified print statement

--- working_with_lists/creating_list.py
+++ working_with_lists/creating_list.py
@@ -1,7 +1,10 @@
+#creating a list by taking user input
+#using for loop and range function
+
 numbers = []
 
 for i in range(5):
-    num = int(input("Enter number: "))
+    num = int(input("Enter numbers: "))
     numbers.append(num)
 
 print(numbers)
