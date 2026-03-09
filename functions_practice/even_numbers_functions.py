68ab088c72c1ec02a85ae67274ff3681cb601334
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-09 15:24:55 +0530

    Finding even numbers in the list from user input using functions and loops as concept

---
+++ functions_practice/even_numbers_functions.py
@@ -0,0 +1,15 @@
+# Program to find even numbers in a list
+
+original_list = []
+for i in range(4):
+    user_input = int(input("Enter the number: "))
+    original_list.append(user_input)
+print(f"Entered list is: {original_list}")
+
+def even_num(numbers):
+    list_items = []
+    for i in numbers:
+        if i % 2 == 0:
+            list_items.append(i)
+    return list_items
+print(f"Even number/s in the list are: {even_num(original_list)} ")
