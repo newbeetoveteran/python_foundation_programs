7ee2a9401954240add3c002eabfe8e983f8cbc8f
newbeetoveteran <rahul.singh04@zohomail.in>
2026-03-10 14:02:21 +0530

    Program to check the largest number in the list without using max

---
+++ functions_practice/max_num_functions.py
@@ -0,0 +1,17 @@
+# Find the largest number in the list
+# Concept : Using function, loops and append
+
+main_list = []
+
+for i in range(4):
+    user_input = int(input(f"Enter the list item number {i+1}: "))
+    main_list.append(user_input)
+print(f"Entered list is {main_list}")
+
+def max_num(numbers):
+    largest = numbers[0]
+    for i in numbers:
+        if i > largest:
+            largest = i
+    return largest
+print(f"Largest number in {main_list} is {max_num(main_list)}")
