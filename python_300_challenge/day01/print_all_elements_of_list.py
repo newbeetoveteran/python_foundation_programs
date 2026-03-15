20c1e038eccdeb4784e9462b1d9c86c01a95f38f
Rz <rahul.singh04@zohomail.in>
2026-03-15 12:30:18 +0530

    Delete python_300_challenge/day01/print_all_elements_of_list.py

--- python_300_challenge/day01/print_all_elements_of_list.py
+++
@@ -1,9 +0,0 @@
-# Print all elements of a list
-# Concept : list, inout from user and loop
-
-user_entered_list = []
-
-for i in range (4):
-    user_input_list = int(input(f"Enter list element {i+1}: "))
-    user_entered_list.append(user_input_list)
-print(f"User entered elements of the list are: {user_entered_list}")
